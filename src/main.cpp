f39e2277c5bfb8fd85211a523a83d7fadfbdfa0b
Knimoms <timon-albrecht@outlook.de>
2023-11-22 23:06:46 +0100

    Compile+CreateShader

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,45 @@
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
-	unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+	unsigned int id = glCreateShader(type);
 	const char* src = source.c_str();
 	glShaderSource(id, 1, &src, nullptr);
 	glCompileShader(id);
 
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*) alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER? "vertex" : "fragment") << " shader!" << std::endl;
+		std::cout << message << std::endl;
+
+		glDeleteShader(id);
+		return 0;
+	}
+
 	return id;
 }
 
-static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
 	unsigned int program = glCreateProgram();
 	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-	unsigned int fs = CompileShader(GL_VERTEX_SHADER, fragmentShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
 }
 
 int main(void)
@@ -29,7 +55,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(1920, 1080, "Knimoms' 3D Engine", glfwGetPrimaryMonitor(), NULL);
+	window = glfwCreateWindow(1920, 1080, "Knimoms' 3D Engine", NULL, NULL);
 
 	if (!window)
 	{
@@ -59,7 +85,31 @@ int main(void)
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	std::string vertexShader =
+		"#version 330 core\n"
+		"#extension GL_ARB_separate_shader_objects : enable\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"gl_Position = position;\n"
+		"}\n";
+
+	std::string fragmentShader =
+		"#version 330 core\n"
+		//"#extension GL_ARB_separate_shader_objects : enable\n"
+		"\n"
+		"layout(location = 0) out vec4 color;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"color = vec4(1.0, 0.0, 0.0, 0.5);\n"
+		"}\n";
+
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	std::cout << shader << std::endl;
+	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
